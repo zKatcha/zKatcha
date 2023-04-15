0a60369d6eaca26dc9c038f8e95a2cdf8b08158d
cheechyuanang <angchee@live.com.my>
2023-04-16 05:56:28 +0900

    fix solidity

--- packages/contracts/contracts/UniversalRelayer.sol
+++ packages/contracts/contracts/UniversalRelayer.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Relayer.sol";
 
 contract UniversalRelayer is Relayer, Ownable {
-    function publishProofVerification(string calldata id, address publisher, string calldata proofIPFSHash, uint256 timestamp, bool isValid) external override onlyOwner {
+    function publishProofVerification(
+        string calldata id,
+        address publisher,
+        string calldata proofIPFSHash,
+        uint256 timestamp,
+        bool isValid
+    ) external override onlyOwner {
         this._publishProofVerification(id, publisher, proofIPFSHash, timestamp, isValid);
     }
 }
--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,15 +1,15 @@
+import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+
 import '@nomiclabs/hardhat-ethers';
 import 'hardhat-deploy';
 import 'dotenv/config';
 
 import { ethers } from 'ethers';
-// import ethers from 'ethers'
+import ethers from 'ethers';
 import { task } from 'hardhat/config';
 import { HardhatConfig, HttpNetworkHDAccountsConfig } from 'hardhat/types';
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -57,19 +57,16 @@ if (useMnemonic) {
     mnemonic: process.env.MNEMONIC,
   };
 } else {
-  ACCOUNT = [PRIVATE_KEY];
+  ACCOUNT = [`0x${PRIVATE_KEY}`];
 }
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     localhost: {
       url: 'http://0.0.0.0:8545',
       saveDeployments: true,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
     },
     hardhat: {
       // TODO: Add snapshot block
@@ -86,147 +83,118 @@ module.exports = {
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       chainId: 1,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
       chainId: 4,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     goerli: {
       url: `https://rpc.ankr.com/eth_goerli`,
       chainId: 5,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     matic: {
       url: 'https://polygon-rpc.com',
       chainId: 137,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
     },
     mumbai: {
       url: 'https://matic-mumbai.chainstacklabs.com',
       chainId: 80001,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     optimism_mainnet: {
       url: 'https://mainnet.optimism.io',
       chainId: 10,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     optimism_testnet: {
       url: 'https://goerli.optimism.io',
       chainId: 420,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     arbitrum_mainnet: {
       url: 'https://arb1.arbitrum.io/rpc',
       chainId: 42161,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     arbitrum_testnet: {
       url: 'https://rinkeby.arbitrum.io/rpc',
       chainId: 421611,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     cronos_testnet: {
       url: `https://evm-t3.cronos.org`,
       chainId: 338,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
     },
     cronos_mainnet: {
       url: `https://mainnet.cronoslabs.com/v1/55e37d8975113ae7a44603ef8ce460aa/`,
       chainId: 25,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       gasLimit: 1000000000000,
     },
     scroll_testnet: {
       url: 'https://alpha-rpc.scroll.io/l2',
       chainId: 534353,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     metis_testnet: {
       url: 'https://goerli.gateway.metisdevops.link',
       chainId: 599,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     base_testnet: {
       url: 'https://goerli.base.org',
       chainId: 84531,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     mantle_testnet: {
       url: 'https://rpc.testnet.mantle.xyz',
       chainId: 5001,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     taiko_testnet: {
       url: 'https://rpc.a2.taiko.xyz',
       chainId: 167004,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
     gnosis: {
       url: 'https://rpc.gnosischain.com/',
       chainId: 100,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
       gasPrice: 3000000000,
     },
     gnosis_testnet: {
       url: 'https://rpc.chiadochain.net',
       chainId: 10200,
-      accounts: ACCOUNT,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    alfajore: {
+      url: 'https://alfajores-forno.celo-testnet.org',
+      chainId: 44787,
+      accounts: [PRIVATE_KEY],
       saveDeployments: true,
     },
   },
-  solidity: {
-    compilers: [
-      {
-        version: '0.8.17',
-        settings: {
-          optimizer: {
-            enabled: true,
-          },
-        },
-      },
-      {
-        version: '0.8.11',
-        settings: {
-          optimizer: {
-            enabled: true,
-          },
-        },
-      },
-      {
-        version: '0.8.12',
-        settings: {
-          optimizer: {
-            enabled: true,
-          },
-        },
-      },
-      {
-        version: '0.7.6',
-        settings: {
-          optimizer: {
-            enabled: true,
-          },
-        },
-      },
-    ],
-  },
+  solidity: '0.8.17',
   namedAccounts: {
     deployer: {
       default: 0, // here this will by default take the first account as deployer
@@ -263,3 +231,5 @@ module.exports = {
     },
   },
 };
+
+export default config;
